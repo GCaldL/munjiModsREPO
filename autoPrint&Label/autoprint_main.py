d11cc4d7b1e4635418451e84cdbdb2b4ec256950
set <geoff.c.777@gmail.com>
2023-05-25 16:55:17 +1000

    asdf??

---
+++ autoPrint&Label/autoprint_main.py
@@ -0,0 +1,17 @@
+# imports
+import printerManager
+import folderMonitor
+import labelMaker
+import GUIs
+import configManager
+
+# consts
+configINI = 
+
+
+# init
+setings = SettingsManager.get()
+
+
+
+# main loop
