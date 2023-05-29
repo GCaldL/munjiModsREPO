707db39613a431530d00a3a4080785b921a18393
set <geoff.c.777@gmail.com>
2023-05-29 11:39:44 +1000

    asdf

--- autoShipping/main.py
+++ autoShipping/main.py
@@ -45,6 +45,6 @@ positions = {
 
 ahk = AHK()
 ahk.add_hotkey('.', callback=setLocations)
-ahk.add_hotkey('/', callback=printLabel)
+ahk.add_hotkey('/', callback=getOrder)
 ahk.start_hotkeys()
 ahk.block_forever()
